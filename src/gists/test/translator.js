bf7325a966265093e1dd5403dd85b12ce0f0148b
Andre <andrefmeirelles@gmail.com>
2020-02-20 22:41:13 -0300

    TDD: what should be tested in the translator

---
+++ src/gists/test/translator.js
@@ -0,0 +1,14 @@
+const expect = require('expect.js')
+const sinon = require('sinon')
+
+describe('the gists translator', () => {
+
+    describe('when creating a new gist', () => {
+
+        it('should respond an error if something goes wrong in the controller')
+        
+        it('should respond a 200 HTTP status and the contents the interactor returns')
+
+    })
+
+})
---
+++ src/gists/translator.js
@@ -0,0 +1,9 @@
+
+const translator = {
+    create: async (req, res) => {
+        
+    }
+
+}
+
+module.exports = translator
