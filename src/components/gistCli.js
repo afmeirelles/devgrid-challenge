94037673fd6c5fe0e3d016837814670be41462bd
Andre <andrefmeirelles@gmail.com>
2020-02-25 00:39:35 -0300

    added .env-example strategy

--- src/components/gistCli.js
+++ src/components/gistCli.js
@@ -1,6 +1,13 @@
 const Gists = require('gists')
+const dotenv = require('dotenv')
+
+const { parsed, error } = dotenv.config()
+
+if (error) {
+    throw Error('Missing or invalid .env file')
+}
 
 module.exports = new Gists({
-    username: process.env.GIT_USERNAME || '',
-    password: process.env.GIT_PASSWORD || ''
+    username: parsed.GIT_USERNAME || '',
+    password: parsed.GIT_PASSWORD || ''
 })
